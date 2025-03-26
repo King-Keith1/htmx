848ea2d82e5c94949df2d3be00a6d6678c13a46b
King-Keith1 <keith4321ben@gmail.com>
2025-03-26 11:05:47 +0200

    commit

---
+++ Chapter 6/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+
+const app = express();
+
+// Set static folder
+app.use(express.static('public'));
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// Handle GET request for profile edit
+app.get('/user/:id/edit', (req, res) => {
+    // send an HTML form for editing
+    res.send(`
+    <form hx-put="/user/1" hx-target="this" hx-swap="outerHTML">
+    <div class="mb-3">
+    <label for="name" class="form-label">Name</label>
+    <input type="text" class="form-control" id="name" name="name" value="Greg
+   Lim">
+    </div>
+    <div class="mb-3">
+    <label for="bio" class="form-label">Bio</label>
+    <textarea type="text" class="form-control" id="bio" name="bio">Follower of Christ |
+   Author of Best-selling Amazon Tech Books and Creator of Coding Courses
+    </textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">
+   Save Changes
+    </button>
+    </form>
+    `);
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
